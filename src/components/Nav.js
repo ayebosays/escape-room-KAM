d3f4382bd61c9be63bed0c19688bb23e690e599b
kent auyeung <kentayeung@gmail.com>
2021-03-28 15:03:11 -0700

    Adding CSS styling for navigation and main

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,9 +2,9 @@ const Nav = () => {
     return (
         <nav className="nav-wrapper">
             <ul>
-                <li>Link 1</li>
-                <li>Link 2</li>
-                <li>Link 3</li>
+                <li><a href="#.html">link 1</a></li>
+                <li><a href="#.html">link 2</a></li>
+                <li><a href="#.html">link 3</a></li>
             </ul>
         </nav>
     )
