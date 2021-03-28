b4eb9bd25569171b2e2ed29458129f189a80a2b4
kent auyeung <kentayeung@gmail.com>
2021-03-28 14:52:15 -0700

    Added ReactJS framework - test 1

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+const Nav = () => {
+    return (
+        <nav className="nav-wrapper">
+            <ul>
+                <li>Link 1</li>
+                <li>Link 2</li>
+                <li>Link 3</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
