ffb64bb0a738f16b0fcfab5f16594d9235d0eec2
kent auyeung <kentayeung@gmail.com>
2021-03-28 15:54:19 -0700

    Added About Us styling

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,9 +1,33 @@
 const AboutUs = () => {
     return (
-        <ul>
-            <li>Abel</li>
-            <li>Kent</li>
-            <li>Maria?</li>
+        <ul className="about-us-container">
+            <li className="aboutus-card">
+                <div className="changetoimg">Headshot here</div>
+                <div className="aboutus-contact">
+                    <h3>Abel</h3>
+                    <p>description about dev</p>
+                    <a href="#.html">Social 1</a>
+                    <a href="#.html">Social 2</a>
+                </div>
+            </li>
+            <li className="aboutus-card">
+                <div className="changetoimg">Headshot here</div>
+                <div className="aboutus-contact">
+                    <h3>Kent</h3>
+                    <p>description about dev</p>
+                    <a href="#.html">Social 1</a>
+                    <a href="#.html">Social 2</a>
+                </div>
+            </li>
+            <li className="aboutus-card">
+                <div className="changetoimg">Headshot here</div>
+                <div className="aboutus-contact">
+                    <h3>Maria</h3>
+                    <p>description about dev</p>
+                    <a href="#.html">Social 1</a>
+                    <a href="#.html">Social 2</a>
+                </div>
+            </li>
         </ul>
     )
 }
